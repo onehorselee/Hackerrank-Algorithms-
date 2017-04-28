eeeb76316b1de5b1803954015ef2697973886792
Qian_Li <onehorselee@gmail.com>
2017-04-28 16:52:41 -0400

    Update Mars Exploration.R

--- Strings/Mars Exploration.R
+++ Strings/Mars Exploration.R
@@ -1,6 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 s <- scan("/dev/stdin", what = list(" "))
-s <- s[1]
-sub <- substring(s, seq(1,nchar(s),3), seq(3,nchar(s),3))
-out <- match(sub,"SOS",nomatch =0)
-cat(length(sub)-sum(out))
+s <- s[[1]]
+
+n <- nchar(s)/3
+received <- unlist(strsplit(s, ""))
+sent <- rep(c("S","O","S"), n)
+diff <- sum(! received == sent)
+cat(diff)
